5dd3d3479de5622e863f352db376e8a39302e732
Panagiotis C. <48190405+roydestroy@users.noreply.github.com>
2026-02-16 14:51:32 +0200

    Revert Created At field customizations in Add Contract

--- LanguageSchoolERP.App/Windows/AddContractWindow.xaml.cs
+++ LanguageSchoolERP.App/Windows/AddContractWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using LanguageSchoolERP.App.ViewModels;
 
 namespace LanguageSchoolERP.App.Windows;
@@ -23,13 +21,4 @@ public partial class AddContractWindow : Window
     }
 
     public void Initialize(AddContractInit init) => _vm.Initialize(init);
-
-    private void CreatedAtDatePicker_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        if (sender is not DatePicker datePicker)
-            return;
-
-        if (!datePicker.IsDropDownOpen)
-            datePicker.IsDropDownOpen = true;
-    }
 }
