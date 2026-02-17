5081efda760fc0441e57f19b1f297bcf2bb80cd8
Panagiotis <panos_ned@yahoo.gr>
2026-02-17 12:36:42 +0200

    Added programs db

---
+++ LanguageSchoolERP.Data/Migrations/20260217102243_InitialBaseline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanguageSchoolERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialBaseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
