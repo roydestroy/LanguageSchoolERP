c10861248f8c99c86500bdaa0b1d4f2eea17976f
Panagiotis <panos_ned@yahoo.gr>
2026-02-17 16:52:52 +0200

    Fixed migrations

--- LanguageSchoolERP.Data/Migrations/20260217125115_AddEnrollmentStopFields.Designer.cs
+++ LanguageSchoolERP.Data/Migrations/20260217125115_AddEnrollmentStopFields.Designer.cs
@@ -4,6 +4,7 @@ using LanguageSchoolERP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LanguageSchoolERP.Data.Migrations
 {
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217125115_AddEnrollmentStopFields")]
+    partial class AddEnrollmentStopFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LanguageSchoolERP.Data/Migrations/20260217125115_AddEnrollmentStopFields.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanguageSchoolERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnrollmentStopFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStopped",
+                table: "Enrollments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StopReason",
+                table: "Enrollments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StoppedAmountWaived",
+                table: "Enrollments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StoppedOn",
+                table: "Enrollments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsStopped",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "StopReason",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "StoppedAmountWaived",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "StoppedOn",
+                table: "Enrollments");
+        }
+    }
+}
